ef9a2c8b9defbc0021ccb9bf862ccede3965c321
Thi tran <thi.tran@flomail.net>
2018-05-25 11:36:10 +0700

    init

---
+++ CocoapodDemo/ABCEngine.swift
@@ -0,0 +1,15 @@
+//
+//  ABCEngine.swift
+//  CocoapodDemo
+//
+//  Created by Apple on 5/25/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ABCEngine: NSObject {
+    static func helloWorld() {
+        print("Hello all!!!!")
+    }
+}
